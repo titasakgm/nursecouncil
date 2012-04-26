2510954770f4843d3a85434f51b1d8291286d635
DrToon <drtoon@samsung>
2012-04-27 00:23:21 +0700

    fix bugs in search action: send blank

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Nursecouncil::Application.routes.draw do
+  match "/code_tambons/search/:q" => "code_tambons#search"
+  match "/code_amphoes/search/:q" => "code_amphoes#search"
+  match "/code_provinces/search/:q" => "code_provinces#search"
+  match "/code_prenames/search/:q" => "code_prenames#search"
+  
+  match "/code_tambons/search/" => "code_tambons#index"
+  match "/code_amphoes/search/" => "code_amphoes#index"
+  match "/code_provinces/search/" => "code_provinces#index"
+  
   resources :code_tambons
   resources :code_amphoes
   resources :code_provinces
@@ -6,9 +15,6 @@ Nursecouncil::Application.routes.draw do
   resources :devlogs
   resources :nc_modules
     
-  match "/code_tambons/search/:q" => "code_tambons#search"
-  match "/code_amphoes/search/:q" => "code_amphoes#search"
-  match "/code_provinces/search/:q" => "code_provinces#search"
-  match "/code_prenames/search/:q" => "code_prenames#search"
+
 
 end
